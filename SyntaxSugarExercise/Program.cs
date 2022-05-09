dbb9fbad240d4af973945ee90737661d6c18e281
sstoveri <sstoveri45@gmail.com>
2022-05-09 14:38:28 -0500

    Add project files.

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------Exercise-------------------------------------------------------
+
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + “greater than or equal to nine”;
+            //}
+
+            //Change the code to implement inferred typing, string interpolation, and the ternary operator
+
+            var answer = 4;
+
+            var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+        }
+    }
+}
